2eec0a2f4a10c0e5ed672ed4c37ff2777e9ac8a6
Marco Sopranzi <marco.sopranzi71@gmail.com>
2024-05-09 10:08:30 +0200

    Test

---
+++ piattaformaIdS/src/main/java/it/unicam/piattaformaIdS/PiattaformaIdSApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.piattaformaIdS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiattaformaIdSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiattaformaIdSApplication.class, args);
+	}
+
+}
