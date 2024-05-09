98c0d174a5e6d76262c750a3c3e3ac4e3a878a1b
Marco Sopranzi <marco.sopranzi71@gmail.com>
2024-05-09 16:22:05 +0200

    Inserimento metodi base classe Animatore

--- piattaformaIdS/src/main/java/it/unicam/piattaformaIdS/piattaforma/attori/Animatore.java
+++ piattaformaIdS/src/main/java/it/unicam/piattaformaIdS/piattaforma/attori/Animatore.java
@@ -2,16 +2,25 @@ package it.unicam.piattaformaIdS.piattaforma.attori;
 
 public class Animatore extends Utente {
 
-    private String tema;
-    private int durata;
+    private String temaContest;
+    private int durataContest;
 
     public Animatore(String username, String email, String password) {
         super(username, email, password);
     }
 
     public void creaContest(String tema, int durata) {
-        this.tema = tema;
-        this.durata = durata;
+        this.temaContest = tema;
+        this.durataContest = durata;
+    }
+
+    public String terminaContest() {
+        // TODO: aggiungere logica
+        return "Contest has finished.";
+    }
+
+    public void valutaContest() {
+        // TODO: aggiungere logica
     }
 
     public Animatore getAccount() {
