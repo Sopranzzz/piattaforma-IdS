3733acd16a7cdf2d3fdf90cd9bb616e437c888ad
Marco Sopranzi <marco.sopranzi71@gmail.com>
2024-05-25 15:59:26 +0200

    Aggiunta classi base itinerario e poi

--- piattaformaIdS/src/main/java/it/unicam/piattaformaIdS/piattaforma/contenuti/Itinerario.java
+++ piattaformaIdS/src/main/java/it/unicam/piattaformaIdS/piattaforma/contenuti/Itinerario.java
@@ -2,18 +2,21 @@ package it.unicam.piattaformaIdS.piattaforma.contenuti;
 
 import it.unicam.piattaformaIdS.piattaforma.attori.Utente;
 
-public class Itinerario extends Contenuto {
-    public int percorrenza;
-    public int lunghezza;
+import java.util.List;
 
-    public Itinerario(int percorrenza, int lunghezza, Utente utente, double idContenuto, String nomeContenuto, String descrizioneContenuto) {
+public class Itinerario extends Contenuto {
+    public int durata;
+    public int lunghezza;
+    public List<Long> listaPOI;
+
+    public Itinerario(int durata, int lunghezza, Utente utente, double idContenuto, String nomeContenuto, String descrizioneContenuto) {
         super(utente, idContenuto, nomeContenuto, descrizioneContenuto);
-        this.percorrenza = percorrenza;
+        this.durata = durata;
         this.lunghezza = lunghezza;
     }
 
     public String getPercorrenza() {
-        return "L'itinerario dura: " + this.percorrenza + " ore.";
+        return "L'itinerario dura: " + this.durata + " ore.";
     }
 
     public String getLunghezza() {
---
+++ piattaformaIdS/src/main/java/it/unicam/piattaformaIdS/piattaforma/contenuti/POI.java
@@ -0,0 +1,9 @@
+package it.unicam.piattaformaIdS.piattaforma.contenuti;
+
+import it.unicam.piattaformaIdS.piattaforma.attori.Utente;
+
+public class POI extends Contenuto {
+    public POI(Utente utente, double idContenuto, String nomeContenuto, String descrizioneContenuto) {
+        super(utente, idContenuto, nomeContenuto, descrizioneContenuto);
+    }
+}
